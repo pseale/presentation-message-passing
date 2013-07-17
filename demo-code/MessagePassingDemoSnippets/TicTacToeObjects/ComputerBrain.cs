1c08d308f34a784d038d46d4410dca8bacbd2d19
Peter Seale <peter@pseale.com>
2013-07-16 22:12:16 -0500

    add basic OO tic tac toe

---
+++ demo-code/MessagePassingDemoSnippets/TicTacToeObjects/ComputerBrain.cs
@@ -0,0 +1,24 @@
+﻿namespace TicTacToeObjects
+{
+  public class ComputerBrain
+  {
+    private readonly Board _board;
+
+    public ComputerBrain(Board board)
+    {
+      _board = board;
+    }
+
+    public Move DetermineNextMove()
+    {
+      var board = _board.GetBoardDataForAi();
+      //terrible AI, enjoy
+      for (int i = 0; i < 9; i++)
+        if (board[i / 3, i % 3] == ' ')
+          return new Move(i / 3, i % 3, 'O');
+
+      //failure case, board is full
+      return new Move();
+    }
+  }
+}
---
+++ demo-code/MessagePassingDemoSnippets/TicTacToeObjects/PlayerMoveReader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TicTacToeObjects
+{
+  public class PlayerMoveReader
+  {
+    public Move Read()
+    {
+      Console.Write("Your move (e.g. A1): ");
+      string move = Console.ReadLine();
+      int col = (int)move[0] - (int)'A';
+      int row = Int32.Parse(move[1].ToString()) - 1;
+      return new Move(row, col, 'X');
+    }
+  }
+}
