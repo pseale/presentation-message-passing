9ad0050b6cdba96b8603caccbacba2c01b19de03
Peter Seale <peter@pseale.com>
2013-07-17 14:58:33 -0500

    add message passing demo code; update presentation and notes

---
+++ demo-code/MessagePassingDemoSnippets/Properties/Board.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace MessagePassingDemoSnippets.Properties
+{
+  public class Board
+  {
+    readonly int[,] _board = new int[3,3];
+    public Board()
+    {
+      MessageBus.Subscribe<PlayerMovedMessage>(UpdateBoard);
+    }
+
+    private void UpdateBoard(PlayerMovedMessage message)
+    {
+      //do work
+      _board[message.Row, message.Col] = message.Mark;
+
+      //publish message
+      var boardUpdatedMessage = new BoardUpdatedMessage {Board = CloneBoard()};
+      MessageBus.Publish(boardUpdatedMessage);
+    }
+
+    private int[,] CloneBoard()
+    {
+      var clone = new int[3,3];
+      for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+          clone[i, j] = _board[i, j];
+      return clone;
+    }
+  }
+
+  public class BoardUpdatedMessage
+  {
+    public int[,] Board { get; set; }
+  }
+
+  public class PlayerMovedMessage
+  {
+    public int Row { get; set; }
+    public int Col { get; set; }
+    public char Mark { get; set; } // 'X' or 'O'
+  }
+
+  public static class MessageBus
+  {
+    public static void Subscribe<T>(Action<T> messageRecipient)
+    {
+      //demo, use a real one
+    }
+
+    public static void Publish<T>(T message)
+    {
+      //demo, use a real one
+    }
+  }
+}
